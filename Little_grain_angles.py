0a3aae00a0dc164847ca6294dcf1989f0b4043fd
Hannah Gallagher <hgallagher477@gmail.com>
2025-11-23 19:18:38 -0500

    I am piecing together what I do for the first small grain size and what the diffuse sphere actually does.

---
+++ Little_grain_angles.py
@@ -0,0 +1,17 @@
+#This little grain is more reflective...we are looking at the spectra and
+# stuff of this at different angles with the goniometer.
+
+#To keep track of all of these files, I could just import them all and decide to separate them if necessary...
+
+
+#Unfortunately, radiometry is difficult for large view zenith angles due to the
+#resulting smaller projected sample areas, so these radiance measurements only extend
+#to view zenith angles of θr = 70◦.
+
+#This one would be the 63-300 micron meters aka mu m.
+
+#There is the diffuse sphere one for the small grains
+#Then i30 and i60 for this grain size with DN spectra and Radiance spectra
+
+#That makes enough sense that we need to use what we did in part 1 if this is part 2, we are
+#using an emperical way of doing things from our line graph thing with some slope ideas...
